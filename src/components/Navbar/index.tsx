8baddddc83eb23e2cac6aed228a82f3180f00251
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-02 21:14:25 -0300

    build: fix imports :rocket:

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,6 @@
-import { Me } from '../../me';
 import { NavbarContainer, NavInfo, Date } from './styles';
 
 export const Navbar: React.FC = () => {
-  const { Github, LinkedIn, Twitter } = Me;
   return (
     <NavbarContainer>
       <div>
@@ -11,13 +9,16 @@ export const Navbar: React.FC = () => {
           alt="felipe me"
         />
         <NavInfo>
-          <a href={Twitter} target="_blank">
+          <a href="https://twitter.com/lainwired_" target="_blank">
             Twitter
           </a>
-          <a href={LinkedIn} target="_blank">
+          <a
+            href="https://www.linkedin.com/in/felipe-austriaco-dev/"
+            target="_blank"
+          >
             LinkedIn
           </a>
-          <a href={Github} target="_blank">
+          <a href="https://github.com/w1redl4in" target="_blank">
             Github
           </a>
         </NavInfo>
