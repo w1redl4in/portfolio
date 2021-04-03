0d718d1d90a91de90b4ba9a33cff37416f99befb
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-02 23:50:07 -0300

    fix: margin and padding :pencil2:

--- src/components/Content/Introduction/styles.ts
+++ src/components/Content/Introduction/styles.ts
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 export const IntroductionContainer = styled.section`
   max-width: 1100px;
 
-  margin: auto auto auto 0.5rem;
+  margin: auto;
 
   padding-bottom: 15rem;
+
+  padding: 0 0 15rem 1rem;
 
   display: flex;
   flex-direction: column;
