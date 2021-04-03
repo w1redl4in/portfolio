4996f11ae07f4ff22611c6baee0513249284c6c0
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-02 21:09:12 -0300

    fix: typo :pencil2:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import { Particles } from '../components/Particles';
 import { theme, GlobalStyles } from '../styles/';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Felipe Austríaco - Portfolio</title>
+        <title>Felipe Austríaco - Portfólio</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
