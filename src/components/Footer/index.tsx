78c8d12b34af44035042b01f8da0186d02e0308f
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-05 23:56:10 -0300

    feat: new footer info :sparkles:

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,35 @@
-import { FooterContainer } from './styles';
+import { FooterContainer, SocialMedias } from './styles';
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from 'react-icons/ai';
+import { theme } from '../../styles';
 
 export const Footer: React.FC = () => {
   return (
     <FooterContainer>
-      <h4>Made by Felipe Austríaco - 2021</h4>
+      <h4>Made with 💖 by Felipe Austríaco</h4>
+      <SocialMedias>
+        <a href="https://twitter.com/lainwired_" target="_blank">
+          <AiFillTwitterCircle color={theme.colors.black900} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/felipe-austriaco-dev/"
+          target="_blank"
+        >
+          <AiFillLinkedin color={theme.colors.black900} />
+        </a>
+        <a href="https://github.com/w1redl4in" target="_blank">
+          <AiFillGithub color={theme.colors.black900} />
+        </a>
+        <a href="https://dev.to/w1redl4in" target="_blank">
+          <img
+            src="https://cdn.discordapp.com/attachments/756656687942729731/828409591200481320/dev-black.png"
+            alt="dev to"
+          />
+        </a>
+      </SocialMedias>
     </FooterContainer>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,9 +2,48 @@ import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
   height: 6rem;
-  border-top: 1px solid ${({ theme }) => theme.colors.grey50};
 
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+
+  z-index: 99999;
+  position: absolute;
+  bottom: -10rem;
+  width: 100%;
+`;
+
+export const SocialMedias = styled.div`
+  display: flex;
+  max-width: 440px;
+  width: 100%;
+
+  margin-top: 0.5rem;
+
+  align-items: center;
+  justify-content: space-around;
+
+  a {
+    svg {
+      width: 2rem;
+      height: 2rem;
+      transition: transform 0.2s;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+
+    > img {
+      width: 2.2rem;
+      border-radius: 0;
+      height: 2.2rem;
+      transition: transform 0.2s;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
 `;
