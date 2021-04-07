ebeb079e296d74f7e887b73b271be6a3a8d12780
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-06 22:00:28 -0300

    build: forgot to fix stuff :rocket:

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -2,5 +2,11 @@ import { Project } from './Projects/Project';
 import { projects } from './Projects/Project/data';
 
 export const Content = () => {
-  return projects.map((project) => <Project project={project} />);
+  return (
+    <>
+      {projects.map((project) => (
+        <Project project={project} />
+      ))}
+    </>
+  );
 };
