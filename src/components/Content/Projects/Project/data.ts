176cc2715c39b9b67304da91578a73ecd0e3a575
Felipe <felipe.araujo@hubfintech.com.br>
2022-03-16 11:35:57 -0300

    feat: :sparkles: add git-emoji-cli, midgard and nashbit projects

--- src/components/Content/Projects/Project/data.ts
+++ src/components/Content/Projects/Project/data.ts
@@ -1,413 +1,489 @@
 export const WhitelabelBank = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/263762535763673089/828392877142179840/felipebocal.png',
-  company: 'Hub Fintech',
-  project: 'Internet Banking Whitelabel',
+    "https://cdn.discordapp.com/attachments/263762535763673089/828392877142179840/felipebocal.png",
+  company: "Hub Fintech",
+  project: "Internet Banking Whitelabel",
   description:
-    'This was the most difficult project, but also the most rewarding, we developed a digital bank from scratch with a tight deadline.',
-  time: 'Jun 2020 — Now',
+    "This was the most difficult project, but also the most rewarding, we developed a digital bank from scratch with a tight deadline.",
+  time: "Jun 2020 — Now",
   reverse: false,
   links: {
     HubBank: {
-      path: 'https://hubbank-internetbank.hubfintech.com.br/',
+      path: "https://hubbank-internetbank.hubfintech.com.br/",
     },
     FreteBank: {
-      path: 'https://fretebank-internetbank.hubfintech.com.br/',
+      path: "https://fretebank-internetbank.hubfintech.com.br/",
     },
     Edmond: {
-      path: 'https://edmond-internetbank.hubfintech.com.br/',
+      path: "https://edmond-internetbank.hubfintech.com.br/",
     },
     LifesBank: {
-      path: 'https://lifesbank-internetbank.hubfintech.com.br/',
+      path: "https://lifesbank-internetbank.hubfintech.com.br/",
     },
   },
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
-    'Node.js': {
-      path: '/images/nodejs-icon.svg',
+    "Node.js": {
+      path: "/images/nodejs-icon.svg",
     },
     Express: {
-      path: '/images/express-109.svg',
+      path: "/images/express-109.svg",
     },
     TypeORM: {
-      path: '/images/typeorm.png',
+      path: "/images/typeorm.png",
     },
     Jest: {
-      path: '/images/jest.svg',
+      path: "/images/jest.svg",
     },
     Insomnia: {
-      path: '/images/insomnia.png',
+      path: "/images/insomnia.png",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Ant.Design': {
-      path: '/images/ant-design.svg',
+    "Ant.Design": {
+      path: "/images/ant-design.svg",
     },
-    'Framer Motion': {
-      path: '/images/framer.png',
+    "Framer Motion": {
+      path: "/images/framer.png",
     },
     Redux: {
-      path: '/images/redux.svg',
+      path: "/images/redux.svg",
     },
-    'Socket.io': {
-      path: '/images/socket-io.svg',
+    "Socket.io": {
+      path: "/images/socket-io.svg",
     },
     Lotties: {
-      path: '/images/lottie.png',
+      path: "/images/lottie.png",
     },
     Reactotron: {
-      path: '/images/reactotron.svg',
+      path: "/images/reactotron.svg",
     },
   },
 };
 
 export const TelaUnica = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829470779615346738/unknown.png',
-  company: 'Hub Fintech',
-  project: 'Tela Única',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829470779615346738/unknown.png",
+  company: "Hub Fintech",
+  project: "Tela Única",
   description:
-    'This project was my first connection with the Javascript universe, I migrated from Ruby on Rails to Node.js and then to React. 💝',
-  time: 'Dez 2019 — Fev 2020',
+    "This project was my first connection with the Javascript universe, I migrated from Ruby on Rails to Node.js and then to React. 💝",
+  time: "Dez 2019 — Fev 2020",
   reverse: true,
   icons: {
-    'React.js': {
-      path: '/images/react.svg',
+    "React.js": {
+      path: "/images/react.svg",
     },
-    'Node.js': {
-      path: '/images/nodejs-icon.svg',
+    "Node.js": {
+      path: "/images/nodejs-icon.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Material UI': {
-      path: '/images/material.png',
+    "Material UI": {
+      path: "/images/material.png",
     },
     Redux: {
-      path: '/images/redux.svg',
+      path: "/images/redux.svg",
     },
-    'Redux Saga': {
-      path: '/images/redux-saga.svg',
+    "Redux Saga": {
+      path: "/images/redux-saga.svg",
     },
     Docker: {
-      path: '/images/docker.png',
+      path: "/images/docker.png",
     },
     Rancher: {
-      path: '/images/rancher.svg',
+      path: "/images/rancher.svg",
     },
     Redis: {
-      path: '/images/redis.svg',
+      path: "/images/redis.svg",
     },
   },
 };
 
 export const EcomDosCria = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829470466753822760/unknown.png',
-  company: 'Hobby',
-  project: 'ECOM DOS CRIA',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829470466753822760/unknown.png",
+  company: "Hobby",
+  project: "ECOM DOS CRIA",
   description:
-    'I had an idea to start a project with longtime friends to provide the    experience of being a programmer. It was incredible, I learned a lot    more than I taught. Everything is documented in our Notion.',
-  time: 'Dez 2020 — Now',
+    "I had an idea to start a project with longtime friends to provide the    experience of being a programmer. It was incredible, I learned a lot    more than I taught. Everything is documented in our Notion.",
+  time: "Dez 2020 — Now",
   reverse: false,
   links: {
     Notion: {
-      path: 'https://www.notion.so/w1redl4in/Ecommerce-dos-cria-61a7d6a3640648e081d01ceb9bbb22a6',
+      path: "https://www.notion.so/w1redl4in/Ecommerce-dos-cria-61a7d6a3640648e081d01ceb9bbb22a6",
     },
   },
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
-    'Node.js': {
-      path: '/images/nodejs-icon.svg',
+    "Node.js": {
+      path: "/images/nodejs-icon.svg",
     },
     Express: {
-      path: '/images/express-109.svg',
+      path: "/images/express-109.svg",
     },
     TypeORM: {
-      path: '/images/typeorm.png',
+      path: "/images/typeorm.png",
     },
     Jest: {
-      path: '/images/jest.svg',
+      path: "/images/jest.svg",
     },
     Heroku: {
-      path: '/images/heroku.png',
+      path: "/images/heroku.png",
     },
     Insomnia: {
-      path: '/images/insomnia.png',
+      path: "/images/insomnia.png",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Ant.Design': {
-      path: '/images/ant-design.svg',
+    "Ant.Design": {
+      path: "/images/ant-design.svg",
     },
     Redux: {
-      path: '/images/redux.svg',
+      path: "/images/redux.svg",
     },
     Netlify: {
-      path: '/images/netlify.svg',
+      path: "/images/netlify.svg",
     },
   },
 };
 
 export const Spotify = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829470115543384094/unknown.png',
-  company: 'Hobby',
-  project: 'Spotify Clone',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829470115543384094/unknown.png",
+  company: "Hobby",
+  project: "Spotify Clone",
   description:
-    'This project was created in order to practice the Nextjs framework and display: grid. Currently using next-auth with Spotify Provider and Spotify API.',
-  time: 'April 2021 — Now',
+    "This project was created in order to practice the Nextjs framework and display: grid. Currently using next-auth with Spotify Provider and Spotify API.",
+  time: "April 2021 — Now",
   reverse: true,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
-    'Next.js': {
-      path: '/images/nextjs.svg',
+    "Next.js": {
+      path: "/images/nextjs.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'styled-components': {
-      path: '/images/styled-components.svg',
+    "styled-components": {
+      path: "/images/styled-components.svg",
     },
-    'Next Auth': {
-      path: '/images/next-auth.png',
+    "Next Auth": {
+      path: "/images/next-auth.png",
     },
   },
 };
 
 export const Crunchyroll = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829445570292088893/screenshot-rocks.png',
-  company: 'Hobby',
-  project: 'Crunchyroll UI Clone',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829445570292088893/screenshot-rocks.png",
+  company: "Hobby",
+  project: "Crunchyroll UI Clone",
   description:
-    'Project created with the intention of practicing animations, styled-components and responsive media queries.',
-  time: 'March 2021 — March 2021',
+    "Project created with the intention of practicing animations, styled-components and responsive media queries.",
+  time: "March 2021 — March 2021",
   reverse: false,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Framer Motion': {
-      path: '/images/framer.png',
+    "Framer Motion": {
+      path: "/images/framer.png",
     },
-    'styled-components': {
-      path: '/images/styled-components.svg',
+    "styled-components": {
+      path: "/images/styled-components.svg",
     },
   },
   links: {
-    'Github Pages': {
-      path: 'https://w1redl4in.github.io/crunchyroll-clone/',
+    "Github Pages": {
+      path: "https://w1redl4in.github.io/crunchyroll-clone/",
     },
   },
 };
 
 export const Nubank = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829452654476132402/screenshot-rocks_1.png',
-  company: 'Hobby',
-  project: 'Nubank UI Clone',
-  description: 'Clone of the UI of one of the largest digital banks.',
-  time: 'Oct 2020 — Nov 2020',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829452654476132402/screenshot-rocks_1.png",
+  company: "Hobby",
+  project: "Nubank UI Clone",
+  description: "Clone of the UI of one of the largest digital banks.",
+  time: "Oct 2020 — Nov 2020",
   reverse: true,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'styled-components': {
-      path: '/images/styled-components.svg',
+    "styled-components": {
+      path: "/images/styled-components.svg",
     },
   },
 };
 
 export const IgNews = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829454194251399218/screenshot-rocks_2.png',
-  company: 'Study',
-  project: 'ignews',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829454194251399218/screenshot-rocks_2.png",
+  company: "Study",
+  project: "ignews",
   description: "Chapter III of Rocketseat's Ignite Journey",
-  time: 'Mar 2020 — Apr 2020',
+  time: "Mar 2020 — Apr 2020",
   reverse: false,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Next.js': {
-      path: '/images/nextjs.svg',
+    "Next.js": {
+      path: "/images/nextjs.svg",
     },
-    'Next Auth': {
-      path: '/images/next-auth.png',
+    "Next Auth": {
+      path: "/images/next-auth.png",
     },
   },
 };
 
 export const Worldtrip = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/829919467874025482/unknown.png',
-  company: 'Challenger',
-  project: 'worldtrip',
+    "https://cdn.discordapp.com/attachments/756656687942729731/829919467874025482/unknown.png",
+  company: "Challenger",
+  project: "worldtrip",
   description: "Chapter IV Chalenger of Rocketseat's Ignite Journey",
-  time: 'Apr 2020 — Now',
+  time: "Apr 2020 — Now",
   reverse: true,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Next.js': {
-      path: '/images/nextjs.svg',
+    "Next.js": {
+      path: "/images/nextjs.svg",
     },
-    'Chakra UI': {
-      path: '/images/chakra-ui.png',
+    "Chakra UI": {
+      path: "/images/chakra-ui.png",
     },
   },
   links: {
     Demo: {
-      path: 'https://world-trip.vercel.app/',
+      path: "https://world-trip.vercel.app/",
     },
   },
 };
 
 export const InternetBankingTreasury = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/756656687942729731/837765991210418226/unknown.png',
-  company: 'Hub Fintech',
-  project: 'Internet Banking Whitelabel Treasury Module',
+    "https://cdn.discordapp.com/attachments/756656687942729731/837765991210418226/unknown.png",
+  company: "Hub Fintech",
+  project: "Internet Banking Whitelabel Treasury Module",
   description:
-    'Treasury module for Internet Banking Whitelabel, providing batch payments.',
-  time: 'Dez 2020 — Now',
+    "Treasury module for Internet Banking Whitelabel, providing batch payments.",
+  time: "Dez 2020 — Now",
   reverse: true,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
-    'Node.js': {
-      path: '/images/nodejs-icon.svg',
+    "Node.js": {
+      path: "/images/nodejs-icon.svg",
     },
     Express: {
-      path: '/images/express-109.svg',
+      path: "/images/express-109.svg",
     },
     TypeORM: {
-      path: '/images/typeorm.png',
+      path: "/images/typeorm.png",
     },
     Jest: {
-      path: '/images/jest.svg',
+      path: "/images/jest.svg",
     },
     Insomnia: {
-      path: '/images/insomnia.png',
+      path: "/images/insomnia.png",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Ant.Design': {
-      path: '/images/ant-design.svg',
+    "Ant.Design": {
+      path: "/images/ant-design.svg",
     },
-    'Framer Motion': {
-      path: '/images/framer.png',
+    "Framer Motion": {
+      path: "/images/framer.png",
     },
     Redux: {
-      path: '/images/redux.svg',
+      path: "/images/redux.svg",
     },
-    'Socket.io': {
-      path: '/images/socket-io.svg',
+    "Socket.io": {
+      path: "/images/socket-io.svg",
     },
     Lotties: {
-      path: '/images/lottie.png',
+      path: "/images/lottie.png",
     },
     Reactotron: {
-      path: '/images/reactotron.svg',
+      path: "/images/reactotron.svg",
     },
   },
 };
 
 export const NextWeather = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/693737591606083654/872278408338489404/bc545a88-df26-416c-bd44-02aa42543f24.png',
-  company: 'Hobby',
-  project: 'next-weather',
+    "https://cdn.discordapp.com/attachments/693737591606083654/872278408338489404/bc545a88-df26-416c-bd44-02aa42543f24.png",
+  company: "Hobby",
+  project: "next-weather",
   description:
-    'The main reason for this project was practicing Context API + Next.js, It uses LocalStorage for caching requests and images are shown based of climate temps + hour of the day',
-  time: 'Ago 2021 — Now',
+    "The main reason for this project was practicing Context API + Next.js, It uses LocalStorage for caching requests and images are shown based of climate temps + hour of the day",
+  time: "Ago 2021 — Now",
   reverse: false,
   icons: {
-    'Next.js': {
-      path: '/images/nextjs.svg',
+    "Next.js": {
+      path: "/images/nextjs.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'styled-components': {
-      path: '/images/styled-components.svg',
+    "styled-components": {
+      path: "/images/styled-components.svg",
     },
-    'Unsplash API': {
-      path: '/images/unsplash.jpeg',
+    "Unsplash API": {
+      path: "/images/unsplash.jpeg",
     },
-    'OpenWeather API': {
-      path: '/images/openweather.jpeg',
+    "OpenWeather API": {
+      path: "/images/openweather.jpeg",
     },
   },
   links: {
     Demo: {
-      path: 'https://next-weather-orpin.vercel.app/',
+      path: "https://next-weather-orpin.vercel.app/",
     },
   },
 };
 
 export const Cryptocurrency = {
   projectImage:
-    'https://cdn.discordapp.com/attachments/416946495502286848/934147613358440488/89a45c7e-eb6b-44b9-bab2-4eb832fbc73d.png',
-  company: 'Challenger',
-  project: 'rtk-cryptocurrency',
-  description: "Implementation of ReduxToolkit + ReduxToolkit Query with Geist UI React framework",
-  time: 'Jan 2022 — Jan 2022',
+    "https://cdn.discordapp.com/attachments/416946495502286848/934147613358440488/89a45c7e-eb6b-44b9-bab2-4eb832fbc73d.png",
+  company: "Challenger",
+  project: "rtk-cryptocurrency",
+  description:
+    "Implementation of ReduxToolkit + ReduxToolkit Query with Geist UI React framework",
+  time: "Jan 2022 — Jan 2022",
   reverse: true,
   icons: {
     React: {
-      path: '/images/react.svg',
+      path: "/images/react.svg",
     },
     TypeScript: {
-      path: '/images/typescript.svg',
+      path: "/images/typescript.svg",
     },
-    'Next.js': {
-      path: '/images/nextjs.svg',
+    "Next.js": {
+      path: "/images/nextjs.svg",
     },
-    'Geist UI': {
-      path: '/images/geistui.png',
+    "Geist UI": {
+      path: "/images/geistui.png",
     },
-    'Redux Toolkit': {
-      path: '/images/redux.svg',
+    "Redux Toolkit": {
+      path: "/images/redux.svg",
     },
-    'Redux Toolkit Query': {
-      path: '/images/redux.svg',
+    "Redux Toolkit Query": {
+      path: "/images/redux.svg",
     },
-    'Rapid API': {
-      path: '/images/rapid-api.jpg',
+    "Rapid API": {
+      path: "/images/rapid-api.jpg",
     },
   },
   links: {
     Demo: {
-      path: 'https://cryptocurrency-rtk.vercel.app/',
+      path: "https://cryptocurrency-rtk.vercel.app/",
     },
   },
+};
+
+export const GitEmojiCLI = {
+  projectImage:
+    "https://cdn.discordapp.com/attachments/139905137874436096/953656265626771496/git-emoji-cli.gif",
+  company: "Hobby",
+  project: "git-emoji-cli",
+  description:
+    "A lightweight git emoji tool with customized conventional commits.",
+  time: "Mar 2022 — Now",
+  reverse: false,
+  icons: {
+    "Node.js": {
+      path: "/images/nodejs-icon.svg",
+    },
+  },
+  links: {
+    Demo: {
+      path: "https://www.npmjs.com/package/git-emoji-cli",
+    },
+  },
+};
+export const Midgard = {
+  projectImage: "/images/midgard.svg",
+  company: "Study",
+  project: "midgard",
+  description:
+    "Projeto criado para a prática de Design-Patterns, Clean Code e Arquiteturas",
+  time: "Fev 2022 — Now",
+  reverse: true,
+  icons: {
+    TypeScript: {
+      path: "/images/typescript.svg",
+    },
+    Jest: {
+      path: "/images/jest.svg",
+    },
+  },
+  links: {
+    Demo: {
+      path: "https://github.com/w1redl4in/midgard",
+    },
+  },
+};
+
+export const Nashbit = {
+  projectImage:
+    "https://cdn.discordapp.com/attachments/139905137874436096/953662590184206336/unknown.png",
+  company: "Freelancer",
+  project: "Nashbit",
+  description:
+    "A pwa digital bank with mobile-first design and light/dark themes",
+  time: "Fev 2022 — Now",
+  reverse: false,
+  icons: {
+    React: {
+      path: "/images/react.svg",
+    },
+    TypeScript: {
+      path: "/images/typescript.svg",
+    },
+    "Next.js": {
+      path: "/images/nextjs.svg",
+    },
+    "Chakra-UI": {
+      path: "/images/chakra-ui.png",
+    },
+    "Redux Toolkit": {
+      path: "/images/redux.svg",
+    },
+    "Redux Toolkit Query": {
+      path: "/images/redux.svg",
+    },
+  },
+  links: {},
 };
 
 export const projects = [
@@ -422,4 +498,7 @@ export const projects = [
   Worldtrip,
   NextWeather,
   Cryptocurrency,
+  GitEmojiCLI,
+  Midgard,
+  Nashbit,
 ];
